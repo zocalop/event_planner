940acac90489de1121aeb967eaf6e7561b6dbacc
zocalop <jcb2861@gmail.com>
2026-04-28 20:26:36 -0300

    Updated EventPlanner.jsx in main

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -7,6 +7,16 @@ const EventPlanner = () => {
             <header>
                 <h1>Welcome to Event Planner</h1>
             </header>
+            <section className="description">
+            </section>
+            <section className="events_categories">
+            </section>
+            <section className="features">
+            </section>
+            <section className="testimonials">
+            </section>
+            <section className="contact">
+            </section>
         </div>
     );
 };
