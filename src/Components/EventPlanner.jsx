54a36fc534621e8503f01a0fa5d9309604a56fbf
zocalop <jcb2861@gmail.com>
2026-04-28 20:48:42 -0300

    Updated EventPlanner.jsx in main

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -8,10 +8,41 @@ const EventPlanner = () => {
                 <h1>Welcome to Event Planner</h1>
             </header>
             <section className="description">
+               <p>Plan and organize your events effortlessly with Event Planner. From
+                  birthdays to corporate meetings, we've got you covered.</p>
+               <button className="get-started-button">Get Started</button>
             </section>
             <section className="events_categories">
+               <ul>
+                  <h2>Social Events:</h2>
+                     <li>Birthday parties</li>
+                     <li>Anniversary celebrations</li>
+                     <li>Wedding receptions</li>
+                     <li>Baby showers</li>
+                     <li>Graduation parties</li>
+                     <li>Family reunions</li>
+               </ul>
+               <ul>
+                  <h2>Entertainment Events:</h2>
+                     <li>Concerts</li>
+                     <li>Music festivals</li>
+                     <li>Film screenings</li>
+                     <li>Comedy shows</li>
+                     <li>Art exhibitions</li>
+                     <li>Cultural events</li>
+               </ul>
+               <ul>
+                  <h2>Community Events:</h2>
+                     <li>Fundraising events</li>
+                     <li>Charity galas</li>
+                     <li>Volunteer drives</li>
+                     <li>Neighborhood block parties</li>
+                     <li>Community festivals</li>
+                     <li>Cultural celebrations</li>
+               </ul>
             </section>
             <section className="features">
+               
             </section>
             <section className="testimonials">
             </section>
