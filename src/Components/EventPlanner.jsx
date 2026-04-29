06f05e4fed44b7c1685ac71faecf834da55a7269
zocalop <jcb2861@gmail.com>
2026-04-28 21:49:47 -0300

    Updated EventPlanner.jsx

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -52,7 +52,15 @@ const EventPlanner = () => {
                   </ul>
             </section>
             <section className="testimonials">
-               
+               <h2>Testimonials</h2>
+               <div className="testimonial">
+                  <p>"Event Planner made organizing my wedding a breeze.  Highly recommended!"</p>
+                  <p className="author">- Emily Johnson</p>
+               </div>
+               <div className="testimonial">
+                  <p>"I use Event Planner for all my corporate events.  It saves me so much time and effort!"</p>
+                  <p className="author">- John Smith</p>                  
+               </div>
             </section>
             <section className="contact">
             </section>
